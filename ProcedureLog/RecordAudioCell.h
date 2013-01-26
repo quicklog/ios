b4e6c4625deede5ff1a4dcee816fd063a62bbf5a
Andrew Vizor <andrew.vizor@intohand.com>
2013-01-26 13:32:20 +0000

    Record audio cell added, layout altered

---
+++ ProcedureLog/RecordAudioCell.h
@@ -0,0 +1,13 @@
+//
+//  RecordAudioCell.h
+//  ProcedureLog
+//
+//  Created by Andrew Vizor on 26/01/2013.
+//  Copyright (c) 2013 Intohand Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecordAudioCell : UIViewController
+
+@end
---
+++ ProcedureLog/RecordAudioViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecordAudioViewController.h
+//  ProcedureLog
+//
+//  Created by Andrew Vizor on 26/01/2013.
+//  Copyright (c) 2013 Intohand Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecordAudioViewController : UIViewController
+
+@end
