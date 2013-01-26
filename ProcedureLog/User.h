aee2b9fb59ae76f701e08f32e538f4488bf9fb6a
Kieran Gutteridge <kgutteridge@gmail.com>
2013-01-26 16:11:48 +0000

    Base User class

---
+++ ProcedureLog/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  ProcedureLog
+//
+//  Created by Kieran Gutteridge on 26/01/2013.
+//  Copyright (c) 2013 Intohand Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
---
+++ ProcedureLog/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  ProcedureLog
+//
+//  Created by Kieran Gutteridge on 26/01/2013.
+//  Copyright (c) 2013 Intohand Ltd. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
