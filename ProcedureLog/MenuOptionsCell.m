57954ae9d4e4cd34226e2fb7b1ead9e7eac48b3a
Kieran Gutteridge <kgutteridge@gmail.com>
2013-01-26 12:30:05 +0000

    Adding menu options

---
+++ ProcedureLog/MenuOptionsCell.m
@@ -0,0 +1,29 @@
+//
+//  MenuOptionsCell.m
+//  ProcedureLog
+//
+//  Created by Kieran Gutteridge on 26/01/2013.
+//  Copyright (c) 2013 Intohand Ltd. All rights reserved.
+//
+
+#import "MenuOptionsCell.h"
+
+@implementation MenuOptionsCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
